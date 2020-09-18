850af714550d909eda33375efae57cfea7e94e62
jiangjunyu <jiangjunyu@ijovo.com>
2020-09-18 14:24:23 +0800

    Update main.go

--- main.go
+++ main.go
@@ -80,7 +80,7 @@ func main() {
 
 	go func() {
 		for {
-			u, _ := url.Parse("http://192.168.6.51:8022/tag-health")
+			u, _ := url.Parse("http://www.baidu.com")
 			//q := u.Query()
 			//u.RawQuery = q.Encode()
 			res, err := http.Get(u.String())
